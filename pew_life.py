011bdcab0cc0c04b8080c7d6c0c0942cf1ac20df
Szymon Pyżalski <szymon@pythonista.net>
2017-08-19 23:52:31 +0200

    some scrolling

--- pew_life.py
+++ pew_life.py
@@ -9,29 +9,51 @@ def pic_from_board(board):
 
 def main():
     pew.init()
-    board = Board(8, 8)
-    cursor_x = 3
-    cursor_y = 3
-    
+    board = Board(16, 16)
+    scroll_x = 3
+    scroll_y = 3
+    cursor_x = 7
+    cursor_y = 7
+
     while True:
         k = pew.keys()
-        if k & pew.K_UP and cursor_y >= 0:
-            cursor_y -= 1
-        if k & pew.K_DOWN and cursor_x < 8:
-            cursor_y += 1
-        if k & pew.K_LEFT and cursor_y >= 0:
-            cursor_x -= 1
-        if k & pew.K_RIGHT and cursor_y < 8:
-            cursor_x += 1
-        if k & pew.K_O:
-            board.toggle_pixel(cursor_x, cursor_y)
+        o_pressed = k & pew.K_O
+        if k & pew.K_UP:
+            if o_pressed and scroll_y > 0:
+                scroll_y -= 1
+            elif cursor_y > 0:
+                cursor_y -= 1
+        if k & pew.K_DOWN:
+            if o_pressed and scroll_y < board.height - 8 - 1:
+                scroll_y += 1
+            elif cursor_y < board.height - 1:
+                cursor_y += 1
+        if k & pew.K_LEFT:
+            if o_pressed and scroll_x > 0:
+                scroll_x -= 1
+            elif cursor_x > 0:
+                cursor_x -= 1
+        if k & pew.K_RIGHT:
+            if o_pressed and scroll_x < board.height - 8 - 1:
+                scroll_x += 1
+            elif cursor_x < board.width - 1:
+                cursor_x += 1
         if k & pew.K_X:
-            board.step()
-        if k:
+            if o_pressed:
+                board.step()
+            else:
+                board.toggle_pixel(cursor_x, cursor_y)
+        if k & ~pew.K_O:
             time.sleep(0.5)
             pew.keys()
-        pic = pic_from_board(board)
-        pic.pixel(cursor_x, cursor_y, (pic.pixel(cursor_x, cursor_y)) + 2)
+        pic = pew.Pix()
+        board_pic = pic_from_board(board)
+        if not o_pressed:
+            board_pic.pixel(
+                cursor_x, cursor_y, (pic.pixel(cursor_x, cursor_y)) + 2
+            )
+        pic.blit(board_pic, -scroll_x, -scroll_y)
+
         pew.show(pic)
 
 
